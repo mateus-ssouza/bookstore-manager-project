1ba8c187d78af16bc8d1a42592f6344f969e7ab4
Mateus Souza <msouzacrv@gmail.com>
2022-08-04 10:33:08 -0300

    Primeiro commit: cricao da estrutura

---
+++ src/main/java/com/mateussouza/gerenciadelivros/GerenciadelivrosApplication.java
@@ -0,0 +1,13 @@
+package com.mateussouza.gerenciadelivros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadelivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadelivrosApplication.class, args);
+	}
+
+}
